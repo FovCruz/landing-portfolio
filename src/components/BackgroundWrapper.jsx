eea3832600487e2e359e7c1cff073ca0070f43a1
Fabianovc <0f.cruz0@gmail.com>
2024-10-11 23:21:37 -0300

    Updates

--- src/components/BackgroundWrapper.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BackgroundWrapper = ({ children }) => (
-  <div className="relative h-full w-full bg-cover bg-center bg-fixed dark:bg-dark-background bg-light-background">
-    {children}
-  </div>
-);
-
-export default BackgroundWrapper;
